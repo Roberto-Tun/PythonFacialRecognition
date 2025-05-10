73962acd71ac30b8aee617f359a46bbbaf8f77fa
Roberto Tun <rtun2004@gmail.com>
2025-05-10 09:24:33 -0500

    added git ignore for api key

--- app/core/firebase.py
+++ app/core/firebase.py
@@ -1,6 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+from config import API_KEY
 
-cred = credentials.Certificate("facerecognitionapp-a1ca7-firebase-adminsdk-fbsvc-4282f64066.json")
+cred = credentials.Certificate(API_KEY)
 firebase_admin.initialize_app(cred)
 db = firestore.client()

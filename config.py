a92281cd51235eaa3df89267b0ad466191839b08
Roberto <rtun2004@gmail.com>
2025-05-13 18:54:56 -0600

    added config.py

--- config.py
+++ config.py
@@ -1,13 +1,1 @@
-import os
-import json
-
-FIREBASE_KEY_PATH = "firebase_key.json"
-
-# Escribe el contenido a un archivo local si no existe
-if not os.path.exists(FIREBASE_KEY_PATH):
-    firebase_key_content = os.getenv("FIREBASE_KEY")
-    if firebase_key_content:
-        with open(FIREBASE_KEY_PATH, "w") as f:
-            f.write(firebase_key_content)
-
-FIREBASE_KEY = FIREBASE_KEY_PATH
+FIREBASE_KEY = 'kaxten-2090b-firebase-adminsdk-fbsvc-051943d1e1.json'
